237c507c8b2b56d5be99f2af10dade4b42c25637
sc2bat <izar_dero@naver.com>
2024-01-05 17:59:45 +0900

    create base image searching app

---
+++ lib/24_01_05/data/photo_api.dart
@@ -0,0 +1,22 @@
+// photo 를 가져오는 함수
+import 'dart:convert';
+
+import 'package:my_flutter_image_grid_app/24_01_05/model/photo.dart';
+import 'package:my_flutter_image_grid_app/constant/constant.dart';
+import 'package:my_flutter_image_grid_app/logger/simple_logger.dart';
+import 'package:http/http.dart' as http;
+
+class PhotoApi {
+  Future<List<Hit>> fetch(String query) async {
+    final response = await http
+        .get(Uri.parse('$PIXABAY_BASE_URL&key=$PIXABAY_API_KEY&q=$query'));
+
+    if (response.statusCode == 200) {
+      Photo photo = Photo.fromJson(jsonDecode(response.body));
+      return photo.hits;
+    }
+
+    logger.info('qwerasdf get photos response error');
+    throw Exception();
+  }
+}
--- lib/24_01_05/main_change_notifier_provider_page.dart
+++ lib/24_01_05/main_change_notifier_provider_page.dart
@@ -1,15 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_image_grid_app/24_01_05/data/photo_api.dart';
+import 'package:my_flutter_image_grid_app/24_01_05/model/photo.dart';
+import 'package:my_flutter_image_grid_app/24_01_05/ui/widget/image_grid_view_widget.dart';
+import 'package:my_flutter_image_grid_app/logger/simple_logger.dart';
 
-class MainChangeNotifierProviderPage extends StatelessWidget {
+class MainChangeNotifierProviderPage extends StatefulWidget {
   const MainChangeNotifierProviderPage({super.key});
+
+  @override
+  State<MainChangeNotifierProviderPage> createState() =>
+      _MainChangeNotifierProviderPageState();
+}
+
+class _MainChangeNotifierProviderPageState
+    extends State<MainChangeNotifierProviderPage> {
+  // photo 를 담을 list
+  List<Hit> photos = [];
+  // 검색창 문자열 컨트롤러
+  final _searchTextController = TextEditingController();
+
+  bool _isLoading = true;
+
+  void presseSearchButton() async {
+    _isLoading = true;
+    photos = await PhotoApi().fetch(_searchTextController.text);
+    _isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchTextController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChangeNotifierProviderPage'),
+        title: const Text(
+          'ChangeNotifierProviderPage',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.black54,
       ),
-      body: const Center(child: Text('Sample')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SearchBar(
+              controller: _searchTextController,
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
+                EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                ),
+              ),
+              hintText: 'apple',
+              hintStyle: MaterialStateProperty.resolveWith<TextStyle>(
+                (Set<MaterialState> states) {
+                  return TextStyle(
+                    color: states.contains(MaterialState.focused)
+                        ? Colors.blue
+                        : Colors.grey,
+                    fontStyle: FontStyle.italic,
+                  );
+                },
+              ),
+              trailing: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      logger.info('qwerasdf press search button ');
+                      presseSearchButton();
+                    },
+                    child: const Icon(
+                      Icons.search,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            _isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Expanded(
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                      itemCount: 20,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16.0,
+                        mainAxisSpacing: 16.0,
+                      ),
+                      itemBuilder: (context, index) {
+                        return PhotoWidget(hit: photos[index]);
+                      },
+                    ),
+                  ),
+          ],
+        ),
+      ),
     );
   }
 }
